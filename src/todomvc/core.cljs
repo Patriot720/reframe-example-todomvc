15f19218060f36f90decdc2d960c32897c7acefe
patriot720 <cerkin-3@yandex.ru>
2019-08-18 20:11:35 +0300

    remove comments

--- src/todomvc/core.cljs
+++ src/todomvc/core.cljs
@@ -4,31 +4,18 @@
             [reagent.core :as reagent]
             [re-frame.core :refer [dispatch dispatch-sync]]
             [secretary.core :as secretary]
-            [todomvc.events] ;; These two are only required to make the compiler
-            [todomvc.subs]   ;; load them (see docs/App-Structure.md)
+            [todomvc.events]
+            [todomvc.subs]
             [todomvc.views]
             [devtools.core :as devtools])
   (:import [goog History]
            [goog.history EventType]))
 
-;; -- Debugging aids ----------------------------------------------------------
-(devtools/install!)       ;; we love https://github.com/binaryage/cljs-devtools
-(enable-console-print!)   ;; so that println writes to `console.log`
+(devtools/install!)
+(enable-console-print!)
 
-;; TODO add tags and tag filters
-
-;; Put an initial value into app-db.
-;; The event handler for `:initialise-db` can be found in `events.cljs`
-;; Using the sync version of dispatch means that value is in
-;; place before we go onto the next step.
 (dispatch-sync [:initialise-db])
 
-;; -- Routes and History ------------------------------------------------------
-;; Although we use the secretary library below, that's mostly a historical
-;; accident. You might also consider using:
-;;   - https://github.com/DomKM/silk
-;;   - https://github.com/juxt/bidi
-;; We don't have a strong opinion.
 ;;
 (defroute "/" [] (dispatch [:set-showing :all]))
 (defroute "/:filter" [filter] (dispatch [:set-showing (keyword filter)]))
@@ -39,17 +26,7 @@
                    (fn [event] (secretary/dispatch! (.-token event))))
     (.setEnabled true)))
 
-;; -- Entry Point -------------------------------------------------------------
-;; Within ../../resources/public/index.html you'll see this code
-;;    window.onload = function () {
-;;      todomvc.core.main();
-;;    }
-;; So this is the entry function that kicks off the app once the HTML is loaded.
 ;;
 (defn ^:export main
-  []
-  ;; Render the UI into the HTML's <div id="app" /> element
-  ;; The view function `todomvc.views/todo-app` is the
-  ;; root view for the entire UI.
-  (reagent/render [todomvc.views/todo-app]
-    (.getElementById js/document "app")))
+  [] (reagent/render [todomvc.views/todo-app]
+       (.getElementById js/document "app")))
