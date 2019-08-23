20861de3be2a1f7412c3f148d225aa0108efe647
patriot720 <cerkin-3@yandex.ru>
2019-08-23 13:48:57 +0300

    fix previous commit

--- src/todomvc/views/tags.cljs
+++ src/todomvc/views/tags.cljs
@@ -3,17 +3,20 @@
           [todomvc.views :as views]
           [re-frame.core :refer [subscribe dispatch]])
   )
-(defn tag-list [id tags]
+(defn tag-list []
   (let [tag-adding (r/atom false)]
-    [:div
-     [:ul {:class "tags"
-           :on-double-click (fn []
-                              (reset! tag-adding true)
-                              (println @tag-adding))}
-      (for [tag tags]
-        ^{:key tag}
-        [:li
-         {:class "tag"}
-         tag])]
-     (when @tag-adding
-       [:h3 "WTF"])]))
+    (fn [id tags]
+      [:div
+       [:ul {:class "tags"
+             :on-double-click (fn []
+                                (reset! tag-adding true))}
+        (for [tag tags]
+          ^{:key tag}
+          [:li
+           {:class "tag"}
+           tag])]
+       (when @tag-adding
+         [views/todo-input
+          {:class "tag-add"
+           :on-save #(dispatch [:add-tag id %])
+           :on-stop #(reset! tag-adding false)}])])))
